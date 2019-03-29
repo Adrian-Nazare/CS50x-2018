9bb89ab30fe1f0124653a0342344d9bef4dc8424
Adrian-Nazare <Adrian-Nazare@users.noreply.github.com>
2019-03-29 00:56:31 +0000

    20190329T005522Z

---
+++ test.py
@@ -0,0 +1,11 @@
+
+import sys, crypt
+
+def main():
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    password = input("Gimme pass ")
+    salt = str(input("Gimme salt "))
+    print (crypt.crypt(password, salt))
+
+if __name__ == "__main__":
+    main()
